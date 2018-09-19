93bfde6d89b575249aa30d8801fdc944ab8fe55d
tuxotron <rafael@troncoso.org>
2018-09-18 21:17:41 -0400

    cleanup

--- main/app_main.c
+++ main/app_main.c
@@ -35,7 +35,7 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
-    // your_context_t *context = event->context;
+    
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
@@ -52,12 +52,12 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 
         case MQTT_EVENT_SUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            // msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-            // ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
             break;
+
         case MQTT_EVENT_UNSUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
             break;
+
         case MQTT_EVENT_PUBLISHED:
             ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
             break;
@@ -66,11 +66,10 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
             printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
             printf("DATA=%.*s\r\n", event->data_len, event->data);
 
+            // Get the topic name
             char *topic = malloc(event->topic_len);
             memcpy(topic, event->topic, event->topic_len);
-            topic[event->topic_len] = '\0';
-
-            ESP_LOGI(TAG, "topic=%s", topic);
+            topic[event->topic_len] = '\0';;
 
             if (strcmp(topic, LEFT_DOOR_TOPIC) == 0) {
                 gpio_set_level(LEFT_DOOR_GPIO, 0);
@@ -137,7 +136,7 @@ static void wifi_init(void)
 static void mqtt_app_start(void)
 {
     const esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = "mqtt://iot.eclipse.org",
+        .uri = CONFIG_MQTT_SERVER,
         .event_handle = mqtt_event_handler,
         // .user_context = (void *)your_context
     };
@@ -164,6 +163,7 @@ void app_main()
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(LEFT_DOOR_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_direction(RIGHT_DOOR_GPIO, GPIO_MODE_OUTPUT);
+    /* Set these PINs high */
     gpio_set_level(LEFT_DOOR_GPIO, 1);
     gpio_set_level(RIGHT_DOOR_GPIO, 1);
 
